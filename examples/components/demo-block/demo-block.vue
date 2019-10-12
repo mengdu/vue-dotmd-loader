4ee381f3184eef1ddc2fb9c2da7d2ece876c36a1
lanyue <lanyueos@qq.com>
2019-10-12 23:27:54 +0800

    独立 demo-block 组件

---
+++ examples/components/demo-block/demo-block.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="m-demo-block">
+    <div class="demo"><slot></slot></div>
+    <div class="code"><slot name="code"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: Object
+  }
+}
+</script>
---
+++ examples/components/demo-block/index.js
@@ -0,0 +1,4 @@
+import DemoBlock from './demo-block'
+import './demo-block.css'
+
+export default DemoBlock
