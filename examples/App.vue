50228385b9b8fa7435e94f7ab30718c1bca941a7
ygl <lanyueos@qq.com>
2019-10-22 09:15:04 +0800

    Chore  更改单词拼写错误

--- examples/App.vue
+++ examples/App.vue
@@ -3,7 +3,7 @@
   <div class="docs-container">
     <header class="header">
       <h1 class="title">vue-dotmd-loader</h1>
-      <p class="des">用于把 markdown 文件转成 Vue 组件的 webpack laoder 工具包。</p>
+      <p class="des">用于把 markdown 文件转成 Vue 组件的 webpack loader 工具包。</p>
       <div class="links">
         <a href="https://github.com/mengdu/vue-dotmd-loader" class="button button-info button-round" style="width: 120px;">Github</a>
         <a href="https://lanyueos.com" class="button button-white button-round" style="width: 120px;">My Home</a>
